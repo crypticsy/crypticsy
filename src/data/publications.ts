5aa744bd34a2e79d9c8bf0005e4917e1aa296d17
crypticsy <animesh19basnet@gmail.com>
2026-01-15 21:06:09 +0000

    🎁 Added new paper

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -1,5 +1,6 @@
 import himalayanJavaCaseStudyImage from "../assets/publication/himalayanJavaCaseStudy.png";
 import digital_threats from "../assets/publication/digital_threats.jpg";
+import digital_second_opinion from "../assets/publication/digital_second_opinion.png";
 
 export type Publication = {
     title: string;
@@ -11,6 +12,14 @@ export type Publication = {
 };
 
 export const publications: Publication[] = [
+    {
+        title:"Digital Second Opinion for Assessment of the Clinical Risks",
+        subtitle:"This article presents a software that can be used as a digital second opinion assistant for optimization of the clinical pathways using only data from the patient's medical history and basic statistics.",
+        type:"Conference Paper",
+        publishedDate:new Date(2025, 10, 1),
+        publicationURL:"https://www.researchgate.net/publication/398610142_Digital_Second_Opinion_for_Assessment_of_the_Clinical_Risks",
+        publicationImage:digital_second_opinion,
+    },
     {
         title: "APT Attribution Using Deep Reinforcement Learning",
         subtitle:
@@ -28,5 +37,5 @@ export const publications: Publication[] = [
         publishedDate: new Date(2023, 7, 1),
         publicationURL: "https://parthstudent.vc/case-study/",
         publicationImage: himalayanJavaCaseStudyImage,
-    },
+    }
 ];
